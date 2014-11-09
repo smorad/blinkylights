2ccdf9698198acd9383358eb8f47d2b351e75178
paeynivek <paeynivek@gmail.com>
2014-11-09 03:45:37 -0800

    more colors
    ADDED
    -added stage and actor libraries to put "buttons" to change the
    paintbrush color.
    -set up input multiplexor for multiple event handling
    -code comments for both files.
    NOT FINISHED.
    -button class needs to be split into its own file.
    -need to change buttons to use shape rendering to remove png dependency.
    OTHER NOTES.
    -Github keeps changing my file EOL to windows format.
    -buttons depends on red blue green.png in asset folder.
    -no code removed.

--- src/client/Lights/core/src/Globe.java
+++ src/client/Lights/core/src/Globe.java
@@ -19,288 +19,300 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.io.IOException;
 
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+
 public class Globe
 {
 
-	private class VertexData
-	{
-		public VertexData(Vector3 position, Vector2 texture, Vector2 offset)
-		{
-			this.offset = offset;
-			this.position = position;
-			this.texture = texture;
-		}
-
-		public Vector3 position;
-		public Vector2 texture;
-		public Vector2 offset;
-	}
-
-	public class Coord
-	{
-		public Coord(int column, int row, boolean valid)
-		{
-			this.valid = valid;
-			this.column = column;
-			this.row = row;
-		}
-		boolean valid;
-		public int column,row;
-	}
 
 
-	private Mesh mesh;
-  	private ShaderProgram shader;
-	private int columns;
-	private int rows;
-	private float rotation = 90;
-	private boolean dirty = true;
+  private class VertexData
+  {
+    public VertexData(Vector3 position, Vector2 texture, Vector2 offset)
+    {
+      this.offset = offset;
+      this.position = position;
+      this.texture = texture;
+    }
 
-	float step;
-	Texture texture;
+    public Vector3 position;
+    public Vector2 texture;
+    public Vector2 offset;
+  }
 
-	Pixmap pixmap;
-	byte colors[];
+  public class Coord
+  {
+    public Coord(int column, int row, boolean valid)
+    {
+      this.valid = valid;
+      this.column = column;
+      this.row = row;
+    }
+    boolean valid;
+    public int column,row;
+  }
 
-	public void PrintColors()
-	{
 
-		ByteBuffer bytes = pixmap.getPixels();
+  private Mesh mesh;
+  private ShaderProgram shader;
+  private int columns;
+  private int rows;
+  private float rotation = 90;
+  private boolean dirty = true;
+
+  float step;
+  Texture texture;
+
+  Pixmap pixmap; //a raw image in memory as repesented by pixels
+  byte colors[];
 
 
-   	try 
-   	{ 
-		Files.write(Paths.get("/Users/Dave/Dropbox/Classes/115/blinkylights/src/client/Lights/core/src/test.txt"), colors);
+  //write to file. called on click or mouse movement.
+  public void PrintColors()
+  {
+
+    ByteBuffer bytes = pixmap.getPixels();
+
+
+    try 
+    { 
+      Files.write(Paths.get("/Users/Dave/Dropbox/Classes/115/blinkylights/src/client/Lights/core/src/test.txt"), colors);
     } 
     catch (IOException e) 
     { 
-    	System.out.println("Failed to write...");
-	}
-}
+      System.out.println("Failed to write...");
+    }
+  }
 
-	public void SetColor(int r, int g, int b, int a)
-	{
-		for (int i =0; i < rows; i++ ) 
-		{
-			for (int j = 0; j < columns; j++ ) 
-			{
-				SetColorAt(j, i,r, g, b, a);
-			}
-		}
+  //set all the LEDs on the app
+  public void SetColor(int r, int g, int b, int a)
+  {
+    for (int i =0; i < rows; i++ ) 
+    {
+      for (int j = 0; j < columns; j++ ) 
+      {
+        SetColorAt(j, i,r, g, b, a);
+      }
+    }
+
+  }
+
+  //helper function for manual color setting.
+  public void SetColorAt(int column, int row, int r, int g, int b, int a)
+  {
+    byte bb = pixmap.getPixels().get((row + column * rows)*4+0);
+
+    pixmap.getPixels().put((row + column * rows)*4, (byte)r);
+    pixmap.getPixels().put((row + column * rows)*4+1, (byte)g);
+    pixmap.getPixels().put((row + column * rows)*4+2, (byte)b);
+    pixmap.getPixels().put((row + column * rows)*4+3, (byte)a);
+
+    colors[(row + column * rows)*3 + 0] = (byte)r;
+    colors[(row + column * rows)*3 + 1] = (byte)g;
+    colors[(row + column * rows)*3 + 2] = (byte)b;
+
+    dirty = true;
+  }
+
+  //manual color setting function on the globe.
+  public void SetColorAt(Coord coord, int r, int g, int b, int a)
+  {
+    SetColorAt(coord.column, coord.row, r, g, b, a);
+  }
+
+
+  //definitions of the globe object.
+  public Globe(int columns, int rows, float step)
+  {
+
+    this.columns = columns;
+    this.rows = rows;
+    this.step = step;
+    colors = new byte[rows * columns*3];;
+
+    shader = new ShaderProgram
+    (
+    Gdx.files.internal("PositionTextureColor.vs").readString(),
+    Gdx.files.internal("PositionTextureColor.fs").readString()
+    );
+
+
+    int components = 7; //position 3 + texture 2 + offset 2 (for smooth shading)
+    mesh = new Mesh(true, rows*columns*components, rows*columns*6,
+    new VertexAttribute(Usage.Position, 3, "a_position"),
+    new VertexAttribute(Usage.TextureCoordinates, 2, "a_texCoords"),
+    new VertexAttribute(Usage.Position, 2, "a_offset"));       
+
+
+    //A Pixmap is basically a raw image in memory as represented by pixels
+    //Pixmap (row) wide, (column) height) using 8 bytes for Red, Green, Blue and Alpha channels
+    pixmap = new Pixmap( rows, columns, Format.RGBA8888 );
+    pixmap.setColor( 0, 0, 0, 0);  
+    pixmap.fill(); //fills pixmap to all black. (should turn off all LED on actual globe.)
+
+
+    SetColor(255, 255, 255, 255); //sets all LED globe in app to white.
+    SetColorAt(0, 15, 255, 255, 255, 255);
+    SetColorAt(0, 14, 255, 0, 0, 255);
+    SetColorAt(0, 13, 0, 255, 0, 255);
+    SetColorAt(0, 11, 0, 0, 255, 255);
 
-	}
+    texture = new Texture( pixmap );
 
-	public void SetColorAt(int column, int row, int r, int g, int b, int a)
-	{
+    //Generate the vertex data for a sphere
+    VertexData verts[][][] = new VertexData[rows][columns][4];
+    for (int r = 0; r < rows; r++)
+    {
+      for (int c = 0; c < columns; c++)
+      {
 
+        float angle1 = c/(float)columns*3.1416f*2.0f;
+        float angle2 = (c+1)/(float)columns*3.1416f*2.0f;
 
-		byte bb = pixmap.getPixels().get((row + column * rows)*4+0);
+        //Find the radius of this band using x^2 + y^2 = r^2 r = 1 since its a unit circle.
+        float radius1 =  (float)Math.sqrt(1.0f - 4 * ((r)/(float)rows-.5f) * ((r)/(float)rows-.5f));
+        float radius2 =  (float)Math.sqrt(1.0f - 4 * ((r+1)/(float)rows -.5f) * ((r+1)/(float)rows-.5f));
 
-		pixmap.getPixels().put((row + column * rows)*4, (byte)r);
-		pixmap.getPixels().put((row + column * rows)*4+1, (byte)g);
-		pixmap.getPixels().put((row + column * rows)*4+2, (byte)b);
-		pixmap.getPixels().put((row + column * rows)*4+3, (byte)a);
 
-		colors[(row + column * rows)*3 + 0] = (byte)r;
-		colors[(row + column * rows)*3 + 1] = (byte)g;
-		colors[(row + column * rows)*3 + 2] = (byte)b;
+        Vector3 p1 = new Vector3(radius1*(float)Math.cos(angle1), r/(float)rows*2, radius1*(float)Math.sin(angle1));
+        Vector3 p2 = new Vector3(radius1*(float)Math.cos(angle2), r/(float)rows*2, radius1*(float)Math.sin(angle2));
 
-		dirty = true;
-	}
+        Vector3 p3 = new Vector3(radius2*(float)Math.cos(angle1), (r+1)/(float)rows*2, radius2*(float)Math.sin(angle1));
+        Vector3 p4 = new Vector3(radius2*(float)Math.cos(angle2), (r+1)/(float)rows*2, radius2*(float)Math.sin(angle2));
 
-	public void SetColorAt(Coord coord, int r, int g, int b, int a)
-	{
+        Vector2 textureCoord = new Vector2((float)r/(float)rows + .5f/rows, (float)c/(float)columns + .5f/columns);
 
-		SetColorAt(coord.column, coord.row, r, g, b, a);
-	}
+        
+        verts[r][c][0] = new VertexData(p1, textureCoord, new Vector2(-1, -1));
+        verts[r][c][1] = new VertexData(p2, textureCoord, new Vector2(-1, 1));
+        verts[r][c][2] = new VertexData(p3, textureCoord, new Vector2(1, -1));
+        verts[r][c][3] = new VertexData(p4, textureCoord, new Vector2(1, 1));
+      }
+    }
 
+    //6 indicies for 2 triangles to draw 1 quad.
+    short indicies[] = new short[columns*6*rows];
+    int j = 0;
+    for (short i = 0; i < columns*6*rows; i+= 6, j++)
+    {
+      indicies[i+0] = (short)(j*4+0);
+      indicies[i+1] = (short)(j*4+1);
+      indicies[i+2] = (short)(j*4+2);
 
-	public Globe(int columns, int rows, float step)
-	{
+      indicies[i+3] = (short)(j*4+1);
+      indicies[i+4] = (short)(j*4+2);
+      indicies[i+5] = (short)(j*4+3);
+    }
 
+    //Flatten the data into a single block to upload... fuck java's type system
+    float t[] = new float[rows*columns*components*4];
+    for (int r = 0; r < rows; r++) 
+    for (int i = 0; i < columns; i++) 
+    {
+      for (int k = 0; k < 4; k++)
+      { 
+        t[r*columns*4*components + i*4*components + k*components + 0] = verts[r][i][k].position.x;
+        t[r*columns*4*components + i*4*components + k*components + 1] = verts[r][i][k].position.y;
+        t[r*columns*4*components + i*4*components + k*components + 2] = verts[r][i][k].position.z;
 
-		this.columns = columns;
-		this.rows = rows;
-		this.step = step;
-		colors = new byte[rows * columns*3];;
+        t[r*columns*4*components + i*4*components + k*components + 3] = verts[r][i][k].texture.x;
+        t[r*columns*4*components + i*4*components + k*components + 4] = verts[r][i][k].texture.y;
 
-		shader = new ShaderProgram
-		(
-      		Gdx.files.internal("PositionTextureColor.vs").readString(),
-      		Gdx.files.internal("PositionTextureColor.fs").readString()
-      	);
+        t[r*columns*4*components + i*4*components + k*components + 5] = verts[r][i][k].offset.x;
+        t[r*columns*4*components + i*4*components + k*components + 6] = verts[r][i][k].offset.y;
+      }
+    }
+    mesh.setVertices(t);
+    mesh.setIndices(indicies);   
 
+  }
 
- 		int components = 7; //position 3 + texture 2 + offset 2 (for smooth shading)
-	 	mesh = new Mesh(true, rows*columns*components, rows*columns*6,
-	 		new VertexAttribute(Usage.Position, 3, "a_position"),
-	 		new VertexAttribute(Usage.TextureCoordinates, 2, "a_texCoords"),
-	 		new VertexAttribute(Usage.Position, 2, "a_offset"));       
- 
+  //returns the globe coordinates when the mouse is clicked.
+  //this function is not called continuously, only when mouse is held down and coordinates change.
+  public Coord GetCoord(int screenX, int screenY)
+  {
+    Matrix4 mm = new Matrix4().translate(new Vector3(0, -1.f*.85f, 1.f)).rotate(0, 1,0, 0).scale(.85f, .85f,.85f);
 
+    // Matrix4 p = (new Matrix4().setToProjection(.0001f, 1, 90, 1)).mul(mm);
+    Matrix4 p = new Matrix4().mul(mm);
 
-		pixmap = new Pixmap( rows, columns, Format.RGBA8888 );
-		pixmap.setColor( 0, 0, 0, 0);
-		pixmap.fill();
+    ByteBuffer buffer = ByteBuffer.allocateDirect(4).order(ByteOrder.nativeOrder());
 
+    Gdx.gl.glReadPixels( screenX, Gdx.graphics.getHeight()-screenY, 1, 1, GL20.GL_DEPTH_COMPONENT, GL20.GL_FLOAT, buffer);
 
-		SetColor(255, 255, 255, 255);
-		SetColorAt(0, 15, 255, 255, 255, 255);
-		SetColorAt(0, 14, 255, 0, 0, 255);
-		SetColorAt(0, 13, 0, 255, 0, 255);
-		SetColorAt(0, 11, 0, 0, 255, 255);
+    float depth = buffer.getFloat(0);
 
 
-		texture = new Texture( pixmap );
+    float x = ((float)screenX/Gdx.graphics.getWidth() - 0.5f) * 2.0f;
+    float y = ((float)screenY/Gdx.graphics.getHeight() - 0.5f) * 2.0f;
+    float z = (float)Math.cos(x*3.1415f/2.0);
 
+    if (depth != 1.0)
+    {
 
+      Vector3 v = new Vector3(x,y,0);
+      v= v.prj(p.inv());
 
-		//Generate the vertex data for a sphere
-	 	VertexData verts[][][] = new VertexData[rows][columns][4];
-	 	for (int r = 0; r < rows; r++)
-	 	{
-	 		for (int c = 0; c < columns; c++)
-	 		{
 
-				float angle1 = c/(float)columns*3.1416f*2.0f;
-				float angle2 = (c+1)/(float)columns*3.1416f*2.0f;
+      float radius = (float)Math.sqrt(1.0 - (v.y - 1.0)*(v.y - 1.0));
 
-				//Find the radius of this band using x^2 + y^2 = r^2 r = 1 since its a unit circle.
-				float radius1 =  (float)Math.sqrt(1.0f - 4 * ((r)/(float)rows-.5f) * ((r)/(float)rows-.5f));
-				float radius2 =  (float)Math.sqrt(1.0f - 4 * ((r+1)/(float)rows -.5f) * ((r+1)/(float)rows-.5f));
 
+      float angle = (float)Math.acos(v.x/radius);
+      System.out.format("%f\n", angle/3.1415*180);
 
-				Vector3 p1 = new Vector3(radius1*(float)Math.cos(angle1), r/(float)rows*2, radius1*(float)Math.sin(angle1));
-				Vector3 p2 = new Vector3(radius1*(float)Math.cos(angle2), r/(float)rows*2, radius1*(float)Math.sin(angle2));
+      int column = (int)((360 - angle/3.1415*180 + rotation)/2.0) % columns;
 
-				Vector3 p3 = new Vector3(radius2*(float)Math.cos(angle1), (r+1)/(float)rows*2, radius2*(float)Math.sin(angle1));
-				Vector3 p4 = new Vector3(radius2*(float)Math.cos(angle2), (r+1)/(float)rows*2, radius2*(float)Math.sin(angle2));
+      int row = (int)(v.y*rows)/2;
+      row = row >= 28 ? 27 : row;
+      row = rows - 1  - row;
 
-				Vector2 textureCoord = new Vector2((float)r/(float)rows + .5f/rows, (float)c/(float)columns + .5f/columns);
+      System.out.format("%d  %d\n", column, row);
+      return new Coord(column, row, true);
 
-	
-				verts[r][c][0] = new VertexData(p1, textureCoord, new Vector2(-1, -1));
-				verts[r][c][1] = new VertexData(p2, textureCoord, new Vector2(-1, 1));
-				verts[r][c][2] = new VertexData(p3, textureCoord, new Vector2(1, -1));
-				verts[r][c][3] = new VertexData(p4, textureCoord, new Vector2(1, 1));
-	 		}
-	 	}
+    }
 
-	 	//6 indeicies for 2 triangles to draw 1 quad.
- 		short indicies[] = new short[columns*6*rows];
- 		int j = 0;
-		for (short i = 0; i < columns*6*rows; i+= 6, j++)
- 		{
- 			indicies[i+0] = (short)(j*4+0);
- 			indicies[i+1] = (short)(j*4+1);
- 			indicies[i+2] = (short)(j*4+2);
+    return new Coord(-1, -1, false);
+  }
+  
+  //renders the globe
+  public void render() 
+  {
 
- 			indicies[i+3] = (short)(j*4+1);
- 			indicies[i+4] = (short)(j*4+2);
- 			indicies[i+5] = (short)(j*4+3);
- 		}
+    rotation += .1; //speed at which the globe turns.
+    Gdx.gl.glClearColor(.25f, .25f, .25f, 1); //grey background.
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+    Gdx.gl.glEnable(GL20.GL_DEPTH_TEST);
 
- 		//Flatten the data into a single block to upload... fuck java's type system
- 		float t[] = new float[rows*columns*components*4];
- 		for (int r = 0; r < rows; r++) 
- 		for (int i = 0; i < columns; i++) 
- 		{
- 			for (int k = 0; k < 4; k++)
- 			{ 
- 				t[r*columns*4*components + i*4*components + k*components + 0] = verts[r][i][k].position.x;
- 				t[r*columns*4*components + i*4*components + k*components + 1] = verts[r][i][k].position.y;
- 				t[r*columns*4*components + i*4*components + k*components + 2] = verts[r][i][k].position.z;
+    Gdx.gl20.glEnable(GL20.GL_BLEND); //no idea
+    Gdx.gl20.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA); //also no idea
 
- 				t[r*columns*4*components + i*4*components + k*components + 3] = verts[r][i][k].texture.x;
- 				t[r*columns*4*components + i*4*components + k*components + 4] = verts[r][i][k].texture.y;
+    
+    if (dirty)
+    {
+      texture = new Texture( pixmap );	
+      dirty = false;
+    }
 
- 				t[r*columns*4*components + i*4*components + k*components + 5] = verts[r][i][k].offset.x;
- 				t[r*columns*4*components + i*4*components + k*components + 6] = verts[r][i][k].offset.y;
- 			}
- 		}
-      	mesh.setVertices(t);
-      	mesh.setIndices(indicies);   
+    Matrix4 v = new Matrix4().translate(new Vector3(0, -1.f*.85f, 1.f)).rotate(0, 1,0, rotation).scale(.85f, .85f,.85f);
 
-	}
+    //Matrix4 p = (new Matrix4().setToProjection(.0001f, 5, 90, 1)).mul(v);
+    Matrix4 p = new Matrix4().mul(v);
 
-	public Coord GetCoord(int screenX, int screenY)
-	{
 
-		Matrix4 mm = new Matrix4().translate(new Vector3(0, -1.f*.85f, 1.f)).rotate(0, 1,0, 0).scale(.85f, .85f,.85f);
+    texture.bind(); //binds the texture to geometry. (binds LED to globe.)
+    shader.begin();
+    shader.setUniformMatrix(shader.getUniformLocation("viewProjection"), p);
+    //sprite.draw(batch);
+    mesh.render(shader, GL20.GL_TRIANGLES, 0, rows*columns*6);
+    shader.end();
 
-				// Matrix4 p = (new Matrix4().setToProjection(.0001f, 1, 90, 1)).mul(mm);
-	   	Matrix4 p = new Matrix4().mul(mm);
-
-		ByteBuffer buffer = ByteBuffer.allocateDirect(4).order(ByteOrder.nativeOrder());
-
-		Gdx.gl.glReadPixels( screenX, Gdx.graphics.getHeight()-screenY, 1, 1, GL20.GL_DEPTH_COMPONENT, GL20.GL_FLOAT, buffer);
-
-		float depth = buffer.getFloat(0);
-
-
-		float x = ((float)screenX/Gdx.graphics.getWidth() - 0.5f) * 2.0f;
-		float y = ((float)screenY/Gdx.graphics.getHeight() - 0.5f) * 2.0f;
-		float z = (float)Math.cos(x*3.1415f/2.0);
-
-		if (depth != 1.0)
-		{
-
-			Vector3 v = new Vector3(x,y,0);
-			v= v.prj(p.inv());
-
-
-			float radius = (float)Math.sqrt(1.0 - (v.y - 1.0)*(v.y - 1.0));
-
-
-			float angle = (float)Math.acos(v.x/radius);
-			System.out.format("%f\n", angle/3.1415*180);
-
-			int column = (int)((360 - angle/3.1415*180 + rotation)/2.0) % columns;
-
-			int row = (int)(v.y*rows)/2;
-			row = row >= 28 ? 27 : row;
-			row = rows - 1  - row;
-
-			System.out.format("%d  %d\n", column, row);
-			return new Coord(column, row, true);
-
-		}
-
-		return new Coord(-1, -1, false);
-
-	}
-	public void render() 
-	{
-
-		rotation += .1;
-		Gdx.gl.glClearColor(.25f, .25f, .25f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-		Gdx.gl.glEnable(GL20.GL_DEPTH_TEST);
-
-		Gdx.gl20.glEnable(GL20.GL_BLEND);
-        Gdx.gl20.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-
-     
-        if (dirty)
-        {
-       		texture = new Texture( pixmap );	
-       		dirty = false;
-        }
-
-		Matrix4 v = new Matrix4().translate(new Vector3(0, -1.f*.85f, 1.f)).rotate(0, 1,0, rotation).scale(.85f, .85f,.85f);
-
-        //Matrix4 p = (new Matrix4().setToProjection(.0001f, 5, 90, 1)).mul(v);
-        Matrix4 p = new Matrix4().mul(v);
-
-
-        texture.bind();
-		shader.begin();
-		shader.setUniformMatrix(shader.getUniformLocation("viewProjection"), p);
-		//sprite.draw(batch);
-    	mesh.render(shader, GL20.GL_TRIANGLES, 0, rows*columns*6);
-		shader.end();
-
-	}
+  }
 
 
 }
--- src/client/Lights/core/src/Lights.java
+++ src/client/Lights/core/src/Lights.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.InputAdapter;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.Mesh;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.graphics.VertexAttribute;
@@ -16,63 +17,155 @@ import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.math.*;
 import java.nio.*;
 
-public class Lights extends ApplicationAdapter
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.InputProcessor;
+
+public class Lights extends ApplicationAdapter implements ApplicationListener
 {
+  //variables that the color setting function pulls from. default red.
+  int r = 255;  //red
+  int b = 0;     //blue
+  int g = 0;    //greem
+  int a = 255;   //alpha
 
-	
-	Globe globe;
+  //color_button is an Actor
+  public class color_button extends Actor {
+    Texture texture;// = new Texture(Gdx.files.internal("green.png"));
+    float x_position = 0, y_position = 0;
+    public boolean started = false;
+    int color_r, color_g, color_b, color_a;
 
-	@Override
-	public void create () 
-	{
+    public color_button(int w, int x, int y, int z, int index){
+      color_r = w;
+      color_g = x;
+      color_b = y;
+      color_a = z;
+      
+      //hackish way for button image. Eventually will want to change to using shapeRendering
+      //to eliminate textures dependency on png file, but for now this works.
+      if (index == 1) { texture = new Texture(Gdx.files.internal("red.png")); }
+      if (index == 2) { texture = new Texture(Gdx.files.internal("green.png")); }
+      if (index == 3) { texture = new Texture(Gdx.files.internal("blue.png")); }
+      
+      setBounds(x_position, y_position, texture.getWidth(), texture.getHeight());
+      addListener(new InputListener(){
+        public boolean touchDown (InputEvent event, float r, float g, int pointer, int button) {
+          ((color_button)event.getTarget()).started = true;
+          return true;
+        }
+      });
+    }
+    
+    public void set_position(int x, int y){ 
+      x_position = x - texture.getWidth()/2;
+      y_position = y - texture.getHeight()/2;
+      setBounds(x_position, y_position, texture.getWidth(), texture.getHeight());
+    }
+    
+    @Override
+    public void draw(Batch batch, float alpha){
+      batch.draw(texture, x_position, y_position);
+    }
+    
+    @Override
+    public void act(float delta){
+      if(started){
+        r = color_r;
+        g = color_g;
+        b = color_b;
+        a = color_a;
+        started = false; //turns actor so the button can be pressed again.
+      }
+    }
+  }
+  
+  private Stage ui; //ui is my stage in where my actors are placed.
+  Globe globe;
 
-		globe = new Globe(180, 28, .1f);
+  @Override
+  public void create () 
+  {
+    InputMultiplexer im = new InputMultiplexer(); //allows for multiple event handling.
+    ui = new Stage();
+    
+    color_button red = new color_button(255, 0, 0, 255, 1); //create button object
+    red.set_position(30, (int)(Gdx.graphics.getHeight()*.9) );
+    
+    color_button green = new color_button(0, 255, 0, 255, 2);
+    green.set_position(30, (int)(Gdx.graphics.getHeight()*.8));
+    
+    color_button blue = new color_button(0, 0, 255, 255, 3);
+    blue.set_position(30, (int)(Gdx.graphics.getHeight()*.7));
+    
+    //I believe this code is for touch screen capabilities on android platforms. not sure.
+    /*
+    red.setTouchable(Touchable.enabled); 
+    green.setTouchable(Touchable.enabled);
+    blue.setTouchable(Touchable.enabled);
+    */
+    
+    //add the buttons to the stage.
+    ui.addActor(red);
+    ui.addActor(blue);
+    ui.addActor(green);
+    
+    im.addProcessor(ui); //add ui to be processed by input multiplexer
+    
+    
+    
+    globe = new Globe(180, 28, .1f);
+    im.addProcessor(new InputAdapter()
+    {
+      public boolean touchDown(int screenX, int screenY, int pointer, int button) 
+      {
+        Globe.Coord coord = globe.GetCoord(screenX, screenY);
 
- 
-		Gdx.input.setInputProcessor(new InputAdapter() 
-		{
-		    public boolean touchDown(int screenX, int screenY, int pointer, int button) 
-		    {
-		    	Globe.Coord coord = globe.GetCoord(screenX, screenY);
+        if (coord.valid)
+        {
+          globe.SetColorAt(coord, r, g, b, a); //sets color on initial click
+          globe.PrintColors();		
+        }
 
-		    	if (coord.valid)
-		    	{
-		    		globe.SetColorAt(coord, 255, 0, 0, 255);
-		    		globe.PrintColors();		
-		    	}
+        return true;
+      }
 
-				return true;
-		    }
+      public boolean touchDragged(int screenX, int screenY, int pointer) 
+      {
+        Globe.Coord coord = globe.GetCoord(screenX, screenY);
+        
+        if (coord.valid)
+        {
+          globe.SetColorAt(coord, r, g, b, a); //sets color if mouse is held.
+          globe.PrintColors();	
+        }
+        return false;
+      }
 
-    		public boolean touchDragged(int screenX, int screenY, int pointer) 
-    		{
-				Globe.Coord coord = globe.GetCoord(screenX, screenY);
-				
-		    	if (coord.valid)
-		    	{
-		    		globe.SetColorAt(coord, 255, 0, 0, 255);
-		    		globe.PrintColors();	
-		    	}
-				return false;
-    		}
+      public boolean touchUp(int screenX, int screenY, int pointer, int button) 
+      {
+        return false;
+      }
+      
+    });
+    globe.PrintColors();
+    
 
-    		public boolean touchUp(int screenX, int screenY, int pointer, int button) 
-    		{
-        		return false;
-    		}
+    Gdx.input.setInputProcessor(im); //set to process input multiplexer   
+  }
 
-
-
-		});
-
-		globe.PrintColors();
-	}
-
-	@Override
-	public void render () 
-	{
-		globe.render();
-	}
-
-
+  @Override
+  public void render () 
+  {
+    globe.render();
+    
+    ui.act(Gdx.graphics.getDeltaTime()); //allows for actor event handling.
+    ui.draw(); //draws the stage otherwise actors are invisible. functional, but invisible.
+  }
 }
