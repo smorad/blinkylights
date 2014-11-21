8174920fef6c53c85796816c8cb73421c523ceee
David Futscher <daveleroy@hotmail.com>
2014-11-20 19:33:57 -0800

    Broke the printing... but added layers and text

---
+++ src/client/Lights/core/src/Layer.java
@@ -0,0 +1,121 @@
+package com.Lights;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.InputAdapter;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.Mesh;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.graphics.VertexAttribute;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.math.*;
+import java.nio.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.BitmapFont.Glyph;
+import com.badlogic.gdx.graphics.g2d.BitmapFont.BitmapFontData;
+
+import com.badlogic.gdx.files.*;
+import com.badlogic.gdx.Files.FileType;
+
+public class Layer
+{
+
+   Layer(int width, int height)
+   {
+
+      pix = new Pixmap(width, height, Pixmap.Format.RGBA8888);
+      pixels = new byte[width * height * 4];
+      buffer = pix.getPixels();
+
+      this.width = width;
+      this.height = height;
+
+    // load the font
+    FileHandle handle = Gdx.files.getFileHandle("font/font.fnt",
+            FileType.Internal);
+    BitmapFont font = new BitmapFont(handle);
+
+    // get the glypth info
+      data = font.getData();
+      fontPixmap = new Pixmap(Gdx.files.internal(data.imagePaths[0]));
+
+
+      DrawLetter('H', 0);
+      DrawLetter('e', 0);
+      DrawLetter('l', 0);
+      DrawLetter('l', 0);
+      DrawLetter('o', 0);
+
+   }
+
+   BitmapFontData data;
+   Pixmap fontPixmap;
+   float advance = 0;
+   char last;
+
+   void DrawLetter(char letter, int at)
+   {
+       Glyph glyph = data.getGlyph(letter);
+
+      // draw the character onto our base pixmap
+      advance -= glyph.getKerning(last);
+
+      pix.drawPixmap(fontPixmap, (int)advance + (width - glyph.width) / 2, (height - glyph.height) / 2,
+      glyph.srcX, glyph.srcY, glyph.width, glyph.height);
+      advance += glyph.xadvance;    
+
+      last = letter;
+
+   }
+
+   public float x, y;
+   public int width, height;
+
+   private Pixmap pix;
+   private byte pixels[];
+   private ByteBuffer buffer;
+
+   public int GetPixelAt(int at)
+   {
+      return buffer.get(at);
+   }
+   public void SetColor(int r, int g, int b, int a)
+   {
+      for (int i =0; i < width; i++ ) 
+      {
+         for (int j = 0; j < height; j++ ) 
+         {
+            SetColorAt(i, j,r, g, b, a);
+         }
+      }
+   }
+
+
+   //helper function for manual color setting.
+   public void SetColorAt(int x, int yy, int r, int g, int b, int a)
+   {
+   	if(x < 0 || x >= width || y < 0 || y >= height) return;
+
+      int y = (height - 1 - yy);
+      buffer.put((x + y * width)*4 + 0, (byte)r);
+      buffer.put((x + y * width)*4 + 1, (byte)g);
+      buffer.put((x + y * width)*4 + 2, (byte)b);
+      buffer.put((x + y * width)*4 + 3, (byte)a);
+   }
+}
